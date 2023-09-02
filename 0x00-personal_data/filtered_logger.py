47f94fbf278f5117ee062a4877c7ab7d976e58d0
Akanni Emmanuel <akannie33@gmail.com>
2023-09-02 23:06:04 +0100

    0x00 task 0: type-annotated function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(r"{}=.*?{}".format(field, separator),
