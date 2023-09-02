5be7a44e530e486518700647abc57a4daf46de23
Akanni Emmanuel <akannie33@gmail.com>
2023-09-02 23:03:35 +0100

    0x00 task 0: filter logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+filter logger
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str):
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(r"{}=.*?{}".format(field, separator),
+                         f"{field}={redaction}{separator}", message)
+    return message
