321155fdb135791556fdcc1a4afadecad6d0fc3f
Akanni Emmanuel <akannie33@gmail.com>
2023-09-03 01:13:26 +0100

    0x00: fixed bug

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,5 +7,6 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """hash `password`"""
-    hashed = bcrypt.hashpw(bytes(password), bcrypt.gensalt())
+    password = password.encode()
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
