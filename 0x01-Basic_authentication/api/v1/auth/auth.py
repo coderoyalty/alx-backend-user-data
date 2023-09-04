80015695061cbb2cd89d7d3a946ae7ca8799e11a
Akanni Emmanuel <akannie33@gmail.com>
2023-09-04 14:43:46 +0100

    0x01: implement Auth.require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,16 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth
         """
+        if path is None or excluded_paths is None\
+                or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path = f"{path}/"
+
+        if path not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
