c03952162f22041303a4b1657e9764e5a00dc339
Akanni Emmanuel <akannie33@gmail.com>
2023-09-04 19:19:32 +0100

    0x01: fixed pycodestyle error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,16 +30,13 @@ def before_req_handler() -> str:
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
     ]
-    print("Authorization header:", request.headers.get("Authorization", None))
-    print("Request path:", request.path)
-
     authorization_result = auth.authorization_header(request)
     print("Authorization result:", authorization_result)
     if not auth.require_auth(request.path, excluded_paths):
         return None
-    if auth.authorization_header(request) == None:
+    if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) == None:
+    if auth.current_user(request) is None:
         abort(403)
 
 
@@ -68,4 +65,4 @@ def forbidden_handler(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
