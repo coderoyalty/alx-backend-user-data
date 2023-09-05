10b06c90d9bec0f04fb790a4bad1e4477fee990d
Akanni Emmanuel <akannie33@gmail.com>
2023-09-05 16:53:58 +0100

    0x01: extract base64 of the authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,17 @@ class BasicAuth(Auth):
     """
     Basic Authentication implementation
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """
+        returns the base64 part of the "Authorization" header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        splits = authorization_header.split(' ')
+        if len(splits) != 2 or splits[0] != "Basic":
+            return None
+        return splits[-1]
