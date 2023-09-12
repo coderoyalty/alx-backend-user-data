76fda6b175c47997c1a034c66c7a1c6c6ba8a04f
Akanni Emmanuel <akannie33@gmail.com>
2023-09-12 17:44:44 +0100

    0x03: add POST /users endpoint handler

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -16,6 +18,23 @@ def root_get():
     return jsonify({
         "message": "Bienvenue"
     })
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
+
+    return jsonify({
+        "email": user.email, "message": "user created"
+    }), 200
 
 
 if __name__ == "__main__":
