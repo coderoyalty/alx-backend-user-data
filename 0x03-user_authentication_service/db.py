13d7dc3f05414b4ef5a7a5b66970daf6fe4718a0
Akanni Emmanuel <akannie33@gmail.com>
2023-09-12 14:34:01 +0100

    0x03: disabled ORM echo, add method to find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,4 +41,21 @@ class DB:
         except Exception:
             self._session.rollback()
             return None
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find user by an arbitrary keyword arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        columns = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in columns:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
         return user
