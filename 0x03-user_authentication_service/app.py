ec9b423ddf77d383231e9119bcfe022a0219f9ee
Akanni Emmanuel <akannie33@gmail.com>
2023-09-14 23:23:31 +0100

    0x03: logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,9 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import (
+    Flask, jsonify, request, abort, redirect
+)
 from auth import Auth
 
 
@@ -58,6 +60,19 @@ def login():
 
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route('/sessions', methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    DELETE /sessions
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        return abort(403)
+    AUTH.destroy_session(session_id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
