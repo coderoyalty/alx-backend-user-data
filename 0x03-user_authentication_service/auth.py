bd6e05462fb859f8b7c9aac6b2e62d5e4068086e
Akanni Emmanuel <akannie33@gmail.com>
2023-09-14 23:34:17 +0100

    0x03: fixed code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,6 +104,6 @@ class Auth:
         if not user:
             raise ValueError
         token = _generate_uuid()
-        self._db.update_user(reset_token=token)
+        self._db.update_user(user.id, reset_token=token)
 
         return token
