8d9fdc020194ae1a23b1ab3d5e403aef727fe69a
Akanni Emmanuel <akannie33@gmail.com>
2023-09-14 23:48:21 +0100

    0x03: add method to update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,3 +109,16 @@ class Auth:
         self._db.update_user(user.id, reset_token=token)
 
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hash = _hash_password(password)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(user.id, hashed_password=hash)
+        self._db.update_user(user.id, reset_token=None)
+        return None
